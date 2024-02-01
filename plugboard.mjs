62e95fd476fb7a0c073529010aebf74b09b74125
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-02-01 00:55:15 +0000

    added option to add plugs

--- plugboard.mjs
+++ plugboard.mjs
@@ -4,8 +4,31 @@ export class PlugBoard {
   }
 
   plug(letter1, letter2) {
-    this.mapping[letter1] = letter2;
-    this.mapping[letter2] = letter1;
+    if (!this.mapping[letter1] || this.mapping[letter2]) {
+      this.mapping[letter1] = letter2;
+      this.mapping[letter2] = letter1;
+    }
+  }
+  unplug(letter, letter2) {
+    delete this.mapping[letter];
+    delete this.mapping[letter2];
+  }
+  getPlugs() {
+    const plugs = {};
+    const seenPairs = new Set();
+
+    for (const [letter1, letter2] of Object.entries(this.mapping)) {
+      const pair = [letter1, letter2].sort().join(""); // Sort the letters and join them
+      if (!seenPairs.has(pair)) {
+        plugs[letter1] = letter2;
+        seenPairs.add(pair);
+      }
+    }
+
+    return plugs;
+  }
+  isPlugged(letter) {
+    return this.mapping.hasOwnProperty(letter);
   }
   process(letter) {
     // Check if the letter is plugged, if not, return the original letter
